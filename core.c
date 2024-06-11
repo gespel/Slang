f9f34fdecd90e32f853ae7218ff32b836f6b3b8d
gespel <heimbrodt@uni-potsdam.de>
2024-06-11 14:15:34 +0200

    fixed

--- core.c
+++ core.c
@@ -5,13 +5,14 @@
 // Created by sten on 11.06.24.
 //
 int main() {
-  char a[] = "Sten ist fn";
+  char a[] = "Sten ist fn 33";
   int length = 0;
   Token* out = tokenize(a, &length);
   printf("%d tokens!\n", length);
-  printf("%s\n", tokenTypeToString(out[0].tt));
-  printf("%s\n", tokenTypeToString(out[1].tt));
-  printf("%s\n", tokenTypeToString(out[2].tt));
+
+  for(int j = 0; j < length; j++) {
+    printf("%s %s\n", out[j].value, tokenTypeToString(out[j].tt));
+  }
   free(out);
 }
 
@@ -22,18 +23,19 @@ Token* tokenize(char* input, int* length) {
   int i = 0;
   t = strtok(input, " ");
   while(t != NULL) {
-    if(isdigit(t[0])) {
+    printf("%d\n", isdigit(t[0]));
+    if(!isdigit(t[0])) {
+      Token temp;
+      temp.tt = IDENTIFIER;
+      temp.value = t;
+      out[i] = temp;
+    }
+    else {
       Token temp;
       temp.tt = NUMBER;
       temp.value = t;
       out[i] = temp;
     }
-    /*else {
-      Token temp;
-      temp.tt = IDENTIFIER;
-      temp.value = t;
-      out[i] = temp;
-    }*/
 
     i++;
     t = strtok(NULL, " ");
