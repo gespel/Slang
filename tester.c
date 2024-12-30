20bd197cb6c9fd6139517ee294203b724969cb68
gespel <heimbrodt@uni-potsdam.de>
2024-12-30 16:43:45 +0100

    test case not working

--- tester.c
+++ tester.c
@@ -4,7 +4,7 @@
 #include "slang-lib.h"
 
 int main() {
-    char a[] = "x = 1; y = 2; z = 14; a = 3; b = 4; xy = x + y; exp = x + y + b + xy * 2; fn sten() { lina = 3; } sten(); p = (1 + 3) * 2;";
+    char a[] = "x = 1; y = 2; z = 14; a = 3; b = 4; xy = x + y; exp = x + y + b + xy * 2; fn sten() { lina = 3; } sten(); p = (1 + 3) * 2 / 5;";
 
     int length = 0;
     Token* tokens = tokenize(a, &length);
