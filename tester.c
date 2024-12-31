7d3a1f15875b5e7dc4464ecb075bd02178a13357
gespel <heimbrodt@uni-potsdam.de>
2024-12-31 22:08:01 +0100

    found bug

--- tester.c
+++ tester.c
@@ -4,7 +4,7 @@
 #include "slang-lib.h"
 
 int main() {
-    char a[] = "x = 3; y = 2; xy = ((x + y)) * (3 + 1);";
+    char a[] = "x = 3; y = 2; xy = ((x + y)) * (3 + 1); xx = x * (x + x)";
 
     int length = 0;
     Token* tokens = tokenize(a, &length);
