9a365e991bc0af9b81765554e4d55697ee5c5cb9
gespel <heimbrodt@uni-potsdam.de>
2025-01-08 17:29:27 +0100

    macro for library or standalone distinction

--- tester.c
+++ tester.c
@@ -2,6 +2,8 @@
 // Created by stenh
 //
 #include "slang-lib.h"
+//#define LIBRARY
+#ifndef LIBRARY
 
 int main() {
     char a[] = "x = 3; y = 2; xy = ((x + y)) * (3 + 1); xx = x * (x + x); fn fun() { sten = 3; } fun(); fun();";
@@ -19,3 +21,4 @@ int main() {
 
     return 0;
 }
+#endif
