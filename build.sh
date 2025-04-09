d1420b09e423ebe17070484ad89ba09afe1f6ccb
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2025-04-09 21:25:13 +0200

    removed old build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-rm core/src/*.o
-cd build
-make
-cd ..
-make
-#rm /lib/libslang.so
-cp build/libslang.so /lib/libslang.so
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/c/Users/stenh/AudioProgramming/Slang/build/
-./test
