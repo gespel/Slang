3aef6d470398710fdc4fb5d6ff731951e51fbdb0
gespel <heimbrodt@uni-potsdam.de>
2025-05-17 22:26:49 +0200

    better interpreter

--- slang.c
+++ slang.c
@@ -5,27 +5,33 @@
 //#define LIBRARY
 #ifndef LIBRARY
 
-int main() {
-    char p[16384];
-    char buff[2048];
-    FILE *f = fopen("example/test1.slang", "r");
+int main(int argc, char **argv) {
+    if(argc == 2) {
+        char p[16384];
+        char buff[2048];
+        FILE *f = fopen(argv[1], "r");
 
-    while(fgets(buff, 2048, f)) {
-        strcat(p, buff);
+        while(fgets(buff, 2048, f)) {
+            strcat(p, buff);
+        }
+        printf("Read programm: %s\n", p);
+
+        int length = 0;
+        Token* tokens = tokenize(p, &length);
+        //printTokens(tokens, length);
+
+
+        printf("%d tokens!\n", length);
+        SlangInterpreter* main_interpreter = createSlangInterpreter(tokens, length);
+        interpret(main_interpreter);
+        printAllFunctions(main_interpreter);
+        printAllVariables(main_interpreter);
+        free(main_interpreter); 
     }
-    printf("Read programm: %s\n", p);
-
-    int length = 0;
-    Token* tokens = tokenize(p, &length);
-    //printTokens(tokens, length);
-
-
-    printf("%d tokens!\n", length);
-    SlangInterpreter* main_interpreter = createSlangInterpreter(tokens, length);
-    interpret(main_interpreter);
-    printAllFunctions(main_interpreter);
-    printAllVariables(main_interpreter);
-    free(main_interpreter); 
+    else {
+        printf("Wrong number of arguments!\nUsage: slang <file to interpret>\n");
+    }
+    
     
     return 0;
 }
