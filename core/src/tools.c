cb1d3464969a9436d854259418614c0b1a5e0e73
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2025-05-19 20:58:57 +0200

    debug formating

--- core/src/tools.c
+++ core/src/tools.c
@@ -7,8 +7,8 @@ void printDebugMessage(char* message) {
     time_t curtime;
     gettimeofday(&tv, NULL); 
     curtime=tv.tv_sec;
-    strftime(buffer,30,"%m-%d-%Y  %T.", localtime(&curtime));
-    printf("\033[95m%s\033[0m - \033[92mDEBUG\033[0m: %s\n", buffer, message);
+    strftime(buffer,30,"%d-%m-%Y %T", localtime(&curtime));
+    printf("\033[34m%s\033[0m - \033[92mDEBUG\033[0m: %s\n", buffer, message);
 #endif
 }
 
