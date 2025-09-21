42cc455ebdd452ec5711fd438f1d8ebca88791a9
gespel <heimbrodt@uni-potsdam.de>
2025-09-21 21:37:16 +0200

    step sequencer working with magic number for samplerate -> set after parse step

--- modules/stepsequencer/src/stepsequencer.c
+++ modules/stepsequencer/src/stepsequencer.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <strings.h>
+#include <stdio.h>
 
 StepSequencer *createStepSequencer(int sampleRate, int speed, float *steps, int numSteps) {
     StepSequencer *seq = malloc(sizeof(StepSequencer));
@@ -19,7 +20,10 @@ StepSequencer *createStepSequencer(int sampleRate, int speed, float *steps, int 
 }
 
 float getStepSequencerSample(StepSequencer *seq) {
-    if (seq->stepIndex >= (seq->sampleRate/seq->numSteps)) {
+    //printf("sample rate: %d, speed: %d, num steps: %d\n", seq->sampleRate, seq->speed, seq->numSteps);
+    float step_duration = (float)48000/ (float)(seq->numSteps);
+    //printf("Step duration: %f, step index: %d, index: %d", step_duration, seq->stepIndex, seq->index);
+    if ((float)seq->stepIndex >= step_duration) {
         seq->stepIndex = 0;
         seq->index += 1;
     }
