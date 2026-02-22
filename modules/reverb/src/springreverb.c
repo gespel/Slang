1296a5edff5eccd3695f1707aff52c1adb528679
gespel <heimbrodt@uni-potsdam.de>
2026-02-22 21:46:58 +0100

    improved?

--- modules/reverb/src/springreverb.c
+++ modules/reverb/src/springreverb.c
@@ -9,12 +9,12 @@ SpringReverb *createSpringReverb(float allpassSize, float feedback, float diffus
     
     /* Early reflections - simulate first reflections from cave walls */
     int earlyDelays[6] = {
-        sampleRate * 0.0043f,  // 4.3ms
-        sampleRate * 0.0215f,  // 21.5ms
-        sampleRate * 0.0225f,  // 22.5ms
-        sampleRate * 0.0268f,  // 26.8ms
-        sampleRate * 0.0270f,  // 27ms
-        sampleRate * 0.0298f   // 29.8ms
+        sampleRate * 0.019f,   // 19ms
+        sampleRate * 0.027f,   // 27ms
+        sampleRate * 0.031f,   // 31ms
+        sampleRate * 0.037f,   // 37ms
+        sampleRate * 0.041f,   // 41ms
+        sampleRate * 0.043f    // 43ms
     };
     
     for (int i = 0; i < 6; i++) {
@@ -23,16 +23,16 @@ SpringReverb *createSpringReverb(float allpassSize, float feedback, float diffus
         out->earlyReflections[i] = calloc(out->earlySize[i], sizeof(float));
     }
     
-    /* 8 parallel comb filters for dense late reverb (Freeverb-style) */
+    /* 8 parallel comb filters for dense late reverb - MUCH longer delays! */
     int combDelays[8] = {
-        sampleRate * 0.0297f,  // ~30ms
-        sampleRate * 0.0371f,  // ~37ms
-        sampleRate * 0.0411f,  // ~41ms
-        sampleRate * 0.0437f,  // ~44ms
-        sampleRate * 0.0521f,  // ~52ms
-        sampleRate * 0.0617f,  // ~62ms
-        sampleRate * 0.0683f,  // ~68ms
-        sampleRate * 0.0741f   // ~74ms
+        sampleRate * 0.1013f,  // ~101ms
+        sampleRate * 0.1193f,  // ~119ms
+        sampleRate * 0.1277f,  // ~128ms
+        sampleRate * 0.1381f,  // ~138ms
+        sampleRate * 0.1511f,  // ~151ms
+        sampleRate * 0.1667f,  // ~167ms
+        sampleRate * 0.1831f,  // ~183ms
+        sampleRate * 0.1973f   // ~197ms
     };
 
     for (int i = 0; i < 8; i++) {
@@ -42,12 +42,12 @@ SpringReverb *createSpringReverb(float allpassSize, float feedback, float diffus
         out->prevCombOut[i] = 0.0f;
     }
 
-    /* 4 allpass filters in series for dense diffusion */
+    /* 4 allpass filters in series for dense diffusion - also longer */
     int allpassDelays[4] = {
-        sampleRate * 0.0051f,  // 5.1ms
-        sampleRate * 0.0117f,  // 11.7ms
-        sampleRate * 0.0181f,  // 18.1ms
-        sampleRate * 0.0299f   // 29.9ms
+        sampleRate * 0.0089f,  // 8.9ms
+        sampleRate * 0.0099f,  // 9.9ms
+        sampleRate * 0.0322f,  // 32.2ms
+        sampleRate * 0.0377f   // 37.7ms
     };
     
     for (int i = 0; i < 4; i++) {
@@ -57,18 +57,18 @@ SpringReverb *createSpringReverb(float allpassSize, float feedback, float diffus
     }
 
     /* Parameters for natural hall/cave sound */
-    out->feedback = feedback;     // longer decay for cave
-    out->apGain   = diffusion;    // high diffusion for smooth tail
-    out->mix      = drywet;       // dry/wet
-    out->damping  = 0.5f;         // stone absorbs highs
+    out->feedback = feedback * 0.95f;  // scale up for longer decay
+    out->apGain   = diffusion * 0.7f;  // moderate diffusion
+    out->mix      = drywet;            // dry/wet
+    out->damping  = 0.4f;              // stone absorbs highs moderately
 
     return out;
 }
 
 float applySpringReverb(SpringReverb *reverb, float sample) {
-    /* Early reflections - discrete echoes from cave walls */
+    /* Early reflections - discrete echoes from cave walls (subtle) */
     float earlySum = 0.0f;
-    float earlyGains[6] = {0.841f, 0.504f, 0.491f, 0.379f, 0.380f, 0.346f};
+    float earlyGains[6] = {0.4f, 0.35f, 0.3f, 0.25f, 0.2f, 0.15f};
     
     for (int i = 0; i < 6; i++) {
         float early = reverb->earlyReflections[i][reverb->earlyIdx[i]];
@@ -78,7 +78,7 @@ float applySpringReverb(SpringReverb *reverb, float sample) {
     }
     
     /* Prepare input for late reverb (early reflections + direct) */
-    float lateInput = sample + earlySum * 0.3f;
+    float lateInput = sample + earlySum * 0.15f;
     
     /* 8 parallel comb filters with damping for late reverb */
     float combSum = 0.0f;
@@ -110,8 +110,8 @@ float applySpringReverb(SpringReverb *reverb, float sample) {
         diffuseIn = apOut;
     }
 
-    /* Mix early reflections + late reverb tail */
-    float wetSignal = earlySum * 0.3f + diffuseIn * 0.7f;
+    /* Mix early reflections + late reverb tail (mostly tail for smooth sound) */
+    float wetSignal = earlySum * 0.15f + diffuseIn * 0.85f;
     
     /* Dry / Wet mix */
     return sample * (1.0f - reverb->mix) + wetSignal * reverb->mix;
